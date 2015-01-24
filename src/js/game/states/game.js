6116391f62152becb83bde6b15d5f1dbf28ebe48
Jesus Roldan <xebuz18@gmail.com>
2015-01-24 02:15:11 -0300

    Loader

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,4 +1,4 @@
-var DummyObject = require('../modules/dummy');  
+var DummyObject = require('../modules/dummy');
 
 module.exports = function(game) {
 
@@ -7,9 +7,6 @@ module.exports = function(game) {
   gameState.create = function () {
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
-
-    var quote = new DummyObject("lalalalala");
-    quote.alert();
 
   };
 
