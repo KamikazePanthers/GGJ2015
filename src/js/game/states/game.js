9abe0ade9de785b2e93ef1ba6a6a6c3cad4c6e17
gbytz <gbytz.zyt@gmail.com>
2015-01-24 19:40:04 -0300

    Fixing merge xD

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -5,6 +5,8 @@ module.exports = function(game) {
   gameState.create = function () {
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+
+    enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
   };
 
 
