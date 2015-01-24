12451ae47bc511176eac4c843ac55a107af89fa0
gbytz <gbytz.zyt@gmail.com>
2015-01-24 15:39:15 -0300

    No gravity for coins

--- src/js/game/states/leveltest.js
+++ src/js/game/states/leveltest.js
@@ -6,6 +6,7 @@ module.exports = function(game) {
     var text = "Level Test";
     var style = { font: "30px Arial", fill: "#FFF", align: "center" };
     var loading = game.add.text(game.world.centerX-100, game.world.centerY+200, text, style);
+    game.physics.arcade.gravity.y = 250;
 
     map = game.add.tilemap('tile_map');
     map.addTilesetImage('tiles', 'tiles');
@@ -19,15 +20,15 @@ module.exports = function(game) {
 
     coins = game.add.group();
     coins.enableBody = true;
-
     map.createFromObjects('objectLayer', 2, 'coin', 0, true, false, coins);
 
     coins.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3], 10, true);
     coins.callAll('animations.play', 'animations', 'spin');
+    coins.setAll('body.allowGravity', false, false, false, 0, true);
 
     p = game.add.sprite(0, 192, 'player');
     game.physics.enable(p);
-    game.physics.arcade.gravity.y = 250;
+
     p.body.bounce.y = 0.2;
     p.body.linearDamping = 1;
     p.body.collideWorldBounds = true;
