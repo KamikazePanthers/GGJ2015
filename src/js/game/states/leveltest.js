7aacce07034954136bc02f266efc98c47a2be801
gbytz <gbytz.zyt@gmail.com>
2015-01-24 06:09:48 -0300

    Loading test map

--- src/js/game/states/leveltest.js
+++ src/js/game/states/leveltest.js
@@ -7,6 +7,13 @@ module.exports = function(game) {
     var style = { font: "30px Arial", fill: "#FFF", align: "center" };
     var loading = game.add.text(game.world.centerX-100, game.world.centerY+200, text, style);
 
+    var map = game.add.tilemap('tile_map');
+    map.addTilesetImage('tiles', 'tiles');
+
+    var backgroundlayer = map.createLayer("backgroundLayer");
+    var blockedLayer = map.createLayer("blockedLayer");
+
+	backgroundlayer.resizeWorld();
   };
 
   levelState.update = function () {
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -9,6 +9,9 @@ module.exports = function(game) {
 
     // Here we need to add all the media
     game.load.image('logo', 'images/phaser.png#grunt-cache-bust');
+    
+    game.load.tilemap('tile_map', 'maps/test_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'images/tiles.png');
   };
 
   preloader.create = function () {
