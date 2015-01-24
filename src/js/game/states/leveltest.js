64af044fc8b4b77274f8282abcb830ff26c53fe7
gbytz <gbytz.zyt@gmail.com>
2015-01-24 11:57:34 -0300

    Collision between test_map and 'the player' (?)

--- src/js/game/states/leveltest.js
+++ src/js/game/states/leveltest.js
@@ -7,16 +7,47 @@ module.exports = function(game) {
     var style = { font: "30px Arial", fill: "#FFF", align: "center" };
     var loading = game.add.text(game.world.centerX-100, game.world.centerY+200, text, style);
 
-    var map = game.add.tilemap('tile_map');
+    map = game.add.tilemap('tile_map');
     map.addTilesetImage('tiles', 'tiles');
 
-    var backgroundlayer = map.createLayer("backgroundLayer");
-    var blockedLayer = map.createLayer("blockedLayer");
+    backgroundlayer = map.createLayer('backgroundLayer');
+    blockedLayer = map.createLayer('blockedLayer');
 
-	backgroundlayer.resizeWorld();
+    map.setCollisionBetween(3, 10, true, 'blockedLayer');
+
+    backgroundlayer.resizeWorld();
+
+    p = game.add.sprite(0, 192, 'player');
+    game.physics.enable(p);
+    game.physics.arcade.gravity.y = 250;
+    p.body.bounce.y = 0.2;
+    p.body.linearDamping = 1;
+    p.body.collideWorldBounds = true;
+
+    cursors = game.input.keyboard.createCursorKeys();
   };
 
   levelState.update = function () {
+
+    game.physics.arcade.collide(p, blockedLayer);
+
+    p.body.velocity.x = 0;
+    if (cursors.up.isDown)
+    {
+        if (p.body.onFloor())
+        {
+            p.body.velocity.y = -300;
+        }
+    }
+
+    if (cursors.left.isDown)
+    {
+        p.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown)
+    {
+        p.body.velocity.x = 150;
+    }
   }
 
   return levelState;
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -9,9 +9,11 @@ module.exports = function(game) {
 
     // Here we need to add all the media
     game.load.image('logo', 'images/phaser.png#grunt-cache-bust');
-    
+
     game.load.tilemap('tile_map', 'maps/test_map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'images/tiles.png');
+
+    game.load.image('player', 'images/player.png');
   };
 
   preloader.create = function () {
