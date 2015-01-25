3249b305047ee6b54859be5df3a71022b2de59d1
Jesus Roldan <xebuz18@gmail.com>
2015-01-25 10:31:56 -0300

    Gameover

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -14,10 +14,11 @@ module.exports = function(game) {
     game.load.tilemap('level2', 'maps/level2.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.tilemap('level3', 'maps/level3.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.tilemap('level4', 'maps/level4.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles_png', 'images/tileset.png#grunt-cache-bust');
+
 
     game.load.spritesheet('player', 'images/player.png#grunt-cache-bust', 192, 256);
     game.load.spritesheet('coin', 'images/coin.png#grunt-cache-bust', 64, 64);
+    game.load.image('tiles_png', 'images/tileset.png#grunt-cache-bust');
     game.load.image('door', 'images/door.png#grunt-cache-bust');
 
     // Enemies
@@ -25,6 +26,9 @@ module.exports = function(game) {
     game.load.image('enemyjumper', 'images/jumper.png#grunt-cache-bust');
     game.load.image('enemyspider', 'images/spider.png#grunt-cache-bust');
     game.load.spritesheet('enemychase', 'images/ghost.png#grunt-cache-bust', 192, 192);
+
+    // Menues
+    game.load.image('gameover', 'images/gameover.png#grunt-cache-bust');
 
   };
 
